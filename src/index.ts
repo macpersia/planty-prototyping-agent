3a16afd95bfdc1469ddd859aaa4a7fcbaad9e63c
macpersia <macpersia@gmail.com>
2018-07-11 20:48:40 +0800

    WIP...

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-var yeoman = require('yeoman-environment');
-var env = yeoman.createEnv();
+// import * as fs from 'fs';
 
-// env.register(require.resolve('generator-jhipster'), 'jhipster:app');
-// env.run('jhipster:app', {}, done);
-// // done.run('jhipster:app entity', {}, done);
+// const pwd = fs.realpath('.');
+// console.log(`PWD:${pwd}`)
 
-env.lookup(() => { 
-    env.run(
-        'jhipster', { 'skip-install': true }, 
-        (err: any) => {
-            console.log(err);
-        },
-        () => {
-            console.log('done');
-        }
-    );
-});
+const yeoman = require('yeoman-environment');
+const env = yeoman.createEnv();
+
+const done = () => {
+    console.log('done');
+};
+env.register(require.resolve('./generator-jhipster'), 'jhipster:app');
+env.run('jhipster:app', { 'skip-install': true }, done);
+
+// env.lookup(() => { 
+//     env.run(
+//         'jhipster', { 'skip-install': true }, 
+//         (err: any) => {
+//             console.log(err);
+//         },
+//         () => {
+//             console.log('done');
+//         }
+//     );
+// });
