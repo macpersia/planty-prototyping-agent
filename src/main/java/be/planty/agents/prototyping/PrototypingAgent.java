6096e2b3603f0b166d4ef2e74bfb72baca6196be
Hadi Ahmadi <MacPersia@gmail.com>
2018-07-26 23:31:07 +0800

    Branding!

--- src/main/java/be/planty/agents/prototyping/PrototypingAgent.java
+++ src/main/java/be/planty/agents/prototyping/PrototypingAgent.java
@@ -4,6 +4,9 @@ import be.planty.agents.assistant.AssistantAgent;
 
 public class PrototypingAgent extends AssistantAgent {
 
+    public PrototypingAgent() {
+        super(agentName);
+    }
 }
 
 
