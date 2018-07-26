bb12c8c1b58e8e501ae5cde3b70c2dddd7952d47
Hadi Ahmadi <MacPersia@gmail.com>
2018-07-27 02:48:40 +0800

    Added a main method

--- src/main/java/be/planty/agents/prototyping/PrototypingAgentApplication.java
+++ src/main/java/be/planty/agents/prototyping/PrototypingAgentApplication.java
@@ -1,6 +1,7 @@
 package be.planty.agents.prototyping;
 
 import be.planty.agents.assistant.AssistantAgentApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +10,9 @@ public class PrototypingAgentApplication extends AssistantAgentApplication {
     @Override
     protected PrototypingAgent createAgent() {
         return new PrototypingAgent();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypingAgentApplication.class, args);
     }
 }
