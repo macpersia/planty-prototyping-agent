4c51df92fc26d3915c9b8f5f6d63fb8116e8969b
macpersia <macpersia@gmail.com>
2018-09-27 13:47:25 +0800

    Agent template for calling another ReSTful service

--- src/main/java/be/planty/agents/prototyping/PrototypingSessionHandler.java
+++ src/main/java/be/planty/agents/prototyping/PrototypingSessionHandler.java
@@ -6,12 +6,20 @@ import be.planty.models.prototyping.ActionResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 import static be.planty.models.assistant.Constants.PAYLOAD_TYPE_KEY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 public class PrototypingSessionHandler extends AgentSessionHandler {
 
@@ -19,22 +27,76 @@ public class PrototypingSessionHandler extends AgentSessionHandler {
 
     private static final ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
 
+    private static final String BASE_URL = "http://dummy-api-host/api";
+
     @Override
     public void handleFrame(StompHeaders headers, Object payload) {
         //super.handleFrame(headers, payload);
-        if (payload instanceof ActionRequest) {
+        if (payload instanceof ActionRequest
+                || "NewWebAppIntent".equals(((ActionRequest) payload).action)) {
             try {
-                final String prettyJson = objectWriter.writeValueAsString(payload);
+                final var prettyJson = objectWriter.writeValueAsString(payload);
                 logger.info("Received action request: " + prettyJson);
+
+                final var actionRequest = (ActionRequest) payload;
+                final var phoneNumber = actionRequest.parameters.getOrDefault("phoneNumber", "0123456789");
+                final var appName = actionRequest.parameters.get("webAppName");
+
+                final var moreDetailsResponse = new RestTemplate()
+                        .getForEntity(BASE_URL + "/more-details/phone-no/" + phoneNumber, Map.class);
+
+                logger.info("moreDetailsResponse: " + moreDetailsResponse);
+                if (moreDetailsResponse.getStatusCode().isError()) {
+                    sendActionResponse(headers, moreDetailsResponse.getStatusCodeValue());
+                    return;
+                }
+                final var customerId = String.valueOf(moreDetailsResponse.getBody().get("customerId"));
+                final HttpEntity<ObjectNode> creationRequest = newCreationRequest(appName, customerId);
+                final var creationResponse = new RestTemplate()
+                        .postForEntity(BASE_URL + "/app-generator/create", creationRequest, Map.class);
+                logger.info("creationResponse: " + creationResponse);
+                if (creationResponse.getStatusCode().isError()) {
+                    sendActionResponse(headers, creationResponse.getStatusCodeValue());
+                    return;
+                }
+                final var appId = String.valueOf(creationResponse.getBody().get("appId"));
+                final var actionResponse = new ActionResponse<>(HttpStatus.OK.value(), appId);
+                logger.info("actionResponse: " + actionResponse);
+                sendActionResponse(headers, actionResponse);
 
             } catch (JsonProcessingException e) {
                 logger.error(e.getMessage());
             }
-            final var response = new ActionResponse(HttpStatus.OK.value());
-            final StompHeaders newHeaders = createStompHeaders(headers);
-            newHeaders.set(PAYLOAD_TYPE_KEY, response.getClass().getTypeName());
-            logger.info("Sending: " + response);
-            session.send(newHeaders, response);
+        } else {
+            sendActionResponse(headers, HttpStatus.NOT_IMPLEMENTED.value());
         }
+    }
+
+    private HttpEntity<ObjectNode> newCreationRequest(String appName, String customerId) {
+        final var createHeaders = new HttpHeaders();
+        createHeaders.setContentType(APPLICATION_JSON);
+        final var nodeFactory = JsonNodeFactory.instance;
+        final var creationPayload = nodeFactory.objectNode();
+        creationPayload
+                .put("customerId", customerId)
+                .put("appName", appName);
+        return new HttpEntity<>(creationPayload, createHeaders);
+    }
+
+    private void sendActionResponse(StompHeaders headers, Integer statusCode) {
+        sendActionResponse(headers, new ActionResponse(statusCode));
+    }
+
+    private void sendActionResponse(StompHeaders headers, ActionResponse actionResponse) {
+        final var newHeaders = createStompHeaders(headers);
+        newHeaders.set(PAYLOAD_TYPE_KEY, actionResponse.getClass().getTypeName());
+        logger.info("Sending: " + actionResponse + "...");
+        try {
+            logger.info(objectWriter.writeValueAsString(actionResponse));
+
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage());
+        }
+        session.send(newHeaders, actionResponse);
     }
 }
