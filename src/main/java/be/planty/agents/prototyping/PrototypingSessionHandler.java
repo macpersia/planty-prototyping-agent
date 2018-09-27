4b77470f45bffb58cdb8b77b29c76a85fadd3115
Hadi Ahmadi <MacPersia@gmail.com>
2018-09-28 00:15:08 +0800

    Skipping incomplete impl.

--- src/main/java/be/planty/agents/prototyping/PrototypingSessionHandler.java
+++ src/main/java/be/planty/agents/prototyping/PrototypingSessionHandler.java
@@ -31,18 +31,18 @@ public class PrototypingSessionHandler extends AgentSessionHandler {
 
     @Override
     public void handleFrame(StompHeaders headers, Object payload) {
-        super.handleFrame(headers, payload);
+
+        // TODO: remove the following once the implementation is ready
+        if (true) {
+            super.handleFrame(headers, payload);
+            return;
+        }
+
         if (payload instanceof ActionRequest
                 && "NewWebAppIntent".equals(((ActionRequest) payload).action)) {
             try {
                 final var prettyJson = objectWriter.writeValueAsString(payload);
                 logger.info("Received action request: " + prettyJson);
-
-                // TODO: remove the following once the implementation is ready
-                if (true) {
-                    sendActionResponse(headers, HttpStatus.NOT_IMPLEMENTED.value());
-                    return;
-                }
 
                 final var actionRequest = (ActionRequest) payload;
                 final var phoneNumber = actionRequest.parameters.getOrDefault("phoneNumber", "0123456789");
