42423f355d21b691ac3316af3d8da1b6b7f22aad
Hadi Ahmadi <MacPersia@gmail.com>
2018-10-10 08:49:11 +0800

    Adding TestProgressiveResponseIntent

--- src/main/java/be/planty/agents/prototyping/PrototypingSessionHandler.java
+++ src/main/java/be/planty/agents/prototyping/PrototypingSessionHandler.java
@@ -41,7 +41,7 @@ public class PrototypingSessionHandler extends AgentSessionHandler {
                 // TODO: remove the following once the implementation is ready
                 if (true) {
                     try {
-                        Thread.currentThread().sleep(10000);
+                        Thread.currentThread().sleep(5000);
 
                     } catch (InterruptedException e) {
                         logger.error(e.getMessage(), e);
